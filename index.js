3cecff7dc53f2065351a0f60666c808fe9168d2f
Nuqrs <35350498+Nuqrs@users.noreply.github.com>
2018-05-23 17:50:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -110,6 +110,24 @@ bot.on('message', (message) => {
             message.channel.send(list[rand]);
         }
         
+        if(command === "!kick") {
+    if(!message.member.roles.some(r=>["Leader", "Commodore"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permission to use this!");
+    
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("Please mention a valid member of this server.");
+    if(!member.kickable) 
+      return message.reply("You cannot kick this user!");
+    
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";
+    
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+        }
+        
 });
 
 bot.login(process.env.BOT_TOKEN);
