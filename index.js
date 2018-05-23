08b861a915a94885373bba174335cee4169db06d
Nuqrs <35350498+Nuqrs@users.noreply.github.com>
2018-05-23 05:08:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ bot.on('message', (message) => {
         
         const msg = message.content.toLowerCase();
         
+        if(msg == '!update') {
+            message.channel.send('@everyone \nAll commands are no longer case sensitive! Feel free to ask for !HELP or !help or !hElP. \n \nPlease reach out to @Nuqrs if you have any suggestions for new commands! \n \nThat is all. Have a pleasant day.');
+        }
+        
         if(msg == '!help') {
             message.channel.send('You can use these commands: \n \n!clanrules - displays clan rules \n!chatrules - displays server chat rules \n!roe - displays RoE');
         }
