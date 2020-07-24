03191ac34d7dcbe6eacd55d2b56d97deb49d059e
Nuqrs <35350498+Nuqrs@users.noreply.github.com>
2020-07-24 19:41:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -253,13 +253,11 @@ bot.on('message', (message) => {
             let channelid = message.guild.channels.find('name','event-rsvp');
             channelid.fetchMessages({limit: 99}).then(msg => {
             const specMessage = msg.filter(msg => msg.content.includes(b)).map(m=>m.id).join('\n');
-            //let messageid = specMessage.id;
-            channelid.send(`${specMessage}`);
-            //async function edit() {
-            //const message = await channelid.fetchMessage(messageid);
-            //await message.edit(c);
-            //}
-            //edit();
+            async function edit() {
+            const message = await channelid.fetchMessage(specMessage);
+            await message.edit(c);
+            }
+            edit();
             });
             message.delete();
             message.channel.send(`Event has been edited.`);
