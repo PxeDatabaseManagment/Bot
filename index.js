bab66f401ab2f4607e9ee868a106bd4e7fed022d
Nuqrs <35350498+Nuqrs@users.noreply.github.com>
2020-07-24 20:37:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -275,7 +275,7 @@ bot.on('message', (message) => {
             const specMessage = msg.filter(msg => msg.content.includes(b)).map(m=>m.id).join('\n');
             async function edit() {
             const message = await channelid.fetchMessage(specMessage);
-            let reactions = message.reactions.cache.users.cache.id;
+            let reactions = message.reactions.cache.users.cache;
             channelid.send(`${reactions}`);
             }
             edit();
