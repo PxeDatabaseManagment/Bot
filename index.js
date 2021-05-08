87b0ab348fe5a40bc4e868c1c5bdb623cb0ea3a5
Nuqrs <35350498+Nuqrs@users.noreply.github.com>
2021-05-08 01:45:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -188,7 +188,7 @@ bot.on('message', (message) => {
             let nickmember = message.guild.members.cache.find(member => member.displayName === nick);
             let nickmemberid = nickmember.id;
             let memberid = message.guild.members.cache.get(nickmemberid);
-	    message.channel.send(`${nickmember} now has the nickname of ${newnick}.`).then(function (memberid) {
+	    memberid.then(function (memberid) {
             memberid.setNickname(newnick)
             }).catch(function() {
             });
