e520cb7709b1b2595cb99a7efb09b021e899be2a
Nuqrs <35350498+Nuqrs@users.noreply.github.com>
2021-05-08 02:31:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -190,11 +190,11 @@ bot.on('message', (message) => {
             let memberid = message.guild.members.cache.get(nickmemberid);
 	    try{
 	    memberid.setNickname(newnick);
-	    } catch(err) {
+	    } catch(error) {
 	    }
 	    try{
 	    message.channel.send(`${nickmember} now has the nickname of ${newnick}.`);
-	    } catch(err2) {
+	    } catch(error) {
 	    }
         }
         
