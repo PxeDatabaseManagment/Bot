de5893eda90aebb3086a334ae8680ece89803b03
Nuqrs <35350498+Nuqrs@users.noreply.github.com>
2021-05-17 23:45:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -188,13 +188,13 @@ bot.on('message', (message) => {
 	    let nickmember = message.guild.members.cache.find(member => member.displayName === nick);
 	    let nickmemberid = nickmember.id;
             //let memberid = message.guild.members.cache.get(nickmemberid);
-	    if (nickmemberid.length > 0) {
+	    //if (nickmemberid.length > 0) {
 	    //nickmember.setNickname(newnick);
 	    //message.channel.send(`${nickmember} now has the nickname of ${newnick}.`);
 	    message.channel.send(`${nickmemberid}`);
-	    } else {
-	    message.channel.send(`${nick} is not a valid nickname of a user in this server.`);
-	    }
+	    //} else {
+	    //message.channel.send(`${nick} is not a valid nickname of a user in this server.`);
+	    //}
         }
         
         if (commando === "c^c") {
