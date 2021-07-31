ba10e11749502011ab8e7a8f9ba6ccc373a3267d
Nuqrs <35350498+Nuqrs@users.noreply.github.com>
2021-07-31 14:28:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -601,8 +601,12 @@ bot.on('message', (message) => {
 	    channelid.messages.fetch({limit: 99}).then(msg => {
 	    const ospecMessage = msg.filter(msg => msg.embeds[0].description.includes(odescription));
 	    const specMessage = ospecMessage.filter(msg => msg.embeds[0].title.includes(otitle)).map(m=>m.id).join('\n');
-	    message.channel.send(`${specMessage}`);
-            channelid.bulkDelete(specMessage);
+	    async function del() {
+            const message = await channelid.messages.fetch(specMessage);
+            await message.bulkDelete(specMessage);
+            }
+            del();
+            //channelid.bulkDelete(specMessage);
             });
             message.channel.send(`Embed has been deleted.`);
 	    } else {
