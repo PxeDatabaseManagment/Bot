90ef7df37b6703c7b944ae6a1777c909373aa680
Nuqrs <35350498+Nuqrs@users.noreply.github.com>
2021-07-31 13:55:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -571,7 +571,8 @@ bot.on('message', (message) => {
 	        .setTimestamp()
 	        .setFooter(nfooter, 'https://cdn.discordapp.com/emojis/417837304036589568.png?v=1');
 	    channelid.messages.fetch({limit: 99}).then(msg => {
-            const specMessage = msg.filter(msg => msg.embeds[0].description.includes(odescription)).map(m=>m.id).join('\n');
+            const ospecMessage = msg.filter(msg => msg.embeds[0].description.includes(odescription));
+	    const specMessage = ospecMessage.filter(msg => msg.embeds[0].description.includes(otitle)).map(m=>m.id).join('\n');
             async function edit() {
             const message = await channelid.messages.fetch(specMessage);
             await message.edit(exampleEmbed);
