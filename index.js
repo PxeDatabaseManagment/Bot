37c90dc864e237f46b936755caced9fa15725a4f
Nuqrs <35350498+Nuqrs@users.noreply.github.com>
2021-08-18 17:46:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 });
 
 client.on('ready', () => {
-client.user.setPresence({ activities: [{ name: "Phoenix Era", type: "WATCHING" }], status: "online" })
+client.user.setPresence({ activities: [{ name: "Managing Phoenix Era", type: "CUSTOM" }], status: "online" })
 });
 
 client.on('guildMemberAdd', member => {
