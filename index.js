c83e0ea42f4eae9aa1a7dc16022a6e4ea01052e7
PxeDatabaseManagment <91637309+PxeDatabaseManagment@users.noreply.github.com>
2022-03-07 13:45:37 -0500

    changed c^a to only clear full schedule

--- index.js
+++ index.js
@@ -412,43 +412,7 @@ client.on('messageCreate', (message) => {
 					channelid.bulkDelete(fetched);
 				}
 				clear();
-				let channelid2 = message.guild.channels.cache.find(i => i.name === 'rudis-events');
-				async function clear2() {
-					const fetched = await channelid2.messages.fetch({limit: 99});
-					channelid2.bulkDelete(fetched);
-				}
-				clear2();
-				let channelid3 = message.guild.channels.cache.find(i => i.name === 'aquila-events');
-				async function clear3() {
-					const fetched = await channelid3.messages.fetch({limit: 99});
-					channelid3.bulkDelete(fetched);
-				}
-				clear3();
-				let channelid4 = message.guild.channels.cache.find(i => i.name === 'vicarius-events');
-				async function clear4() {
-					const fetched = await channelid4.messages.fetch({limit: 99});
-					channelid4.bulkDelete(fetched);
-				}
-				clear4();
-				let channelid5 = message.guild.channels.cache.find(i => i.name === 'equitum-events');
-				async function clear5() {
-					const fetched = await channelid5.messages.fetch({limit: 99});
-					channelid5.bulkDelete(fetched);
-				}
-				clear5();
-				let channelid6 = message.guild.channels.cache.find(i => i.name === 'pg-events');
-				async function clear6() {
-					const fetched = await channelid6.messages.fetch({limit: 99});
-					channelid6.bulkDelete(fetched);
-				}
-				clear6();
-				let channelid7 = message.guild.channels.cache.find(i => i.name === 'intel-events');
-				async function clear7() {
-					const fetched = await channelid7.messages.fetch({limit: 99});
-					channelid7.bulkDelete(fetched);
-				}
-				clear7();
-				message.channel.send(`Messages in all schedule channels have been cleared.`);
+				message.channel.send(`Messages in full-schedule channel have been cleared.`);
 			} else {
 				message.channel.send(`${chan} is not a valid channel to use this command in, ***DUMBASS***.`);
 			}
